322b0d15974010751a43019c843c03218b5dc6aa
Mun Soft <munsoftwares@gmail.com>
2020-02-28 01:28:51 +0530

    unwanted-local-import-removed

--- src/App/App.js
+++ src/App/App.js
@@ -5,11 +5,12 @@ import AppNavigation from './AppNavbar';
 
 function App() {
   const data = require('../Data/PatientData.json');
+
   // const firstObj = { name: 'Abhijeet', age: 33 };
   // const firstObjStr = JSON.stringify(firstObj);
   // localStorage.setItem('firstObj', firstObjStr);
-  const firstObjFromStorage = localStorage.getItem('firstObj');
-  console.log('*** firstObjFromStorage = ' + JSON.parse(firstObjFromStorage).name);
+  // const firstObjFromStorage = localStorage.getItem('firstObj');
+  // console.log('*** firstObjFromStorage = ' + JSON.parse(firstObjFromStorage).name);
 
   return (
     <div className="App">
