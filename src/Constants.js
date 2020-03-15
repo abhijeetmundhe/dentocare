0344fa54c7a601b47e4762e8d5ef6e49dceba401
Mun Soft <munsoftwares@gmail.com>
2020-03-16 02:27:08 +0530

    proxy-url-added-back

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,7 @@
 export const GET_ALL_PATIENTS_URL = 'https://hyqpqh7av5.execute-api.us-east-2.amazonaws.com/live/getallpatients';
 export const ADD_UPDATE_PATIENT_URL = 'https://hyqpqh7av5.execute-api.us-east-2.amazonaws.com/live/addupdatepatient';
-export const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
-// export const PROXY_URL = '';
+// export const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+export const PROXY_URL = '';
 export const TEST_URL = 'https://api.github.com/users/hadley/orgs';
 export const genderProvider = [
     {
